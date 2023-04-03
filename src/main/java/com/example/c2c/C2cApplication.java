079c3be75e483173dbda9830c35e54e0dcc0b149
JunSeong_Park <ttotto0608@naver.com>
2023-04-03 23:42:34 +0900

    basic token logic

--- src/main/java/com/example/c2c/C2cApplication.java
+++ src/main/java/com/example/c2c/C2cApplication.java
@@ -2,6 +2,9 @@ package com.example.c2c;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class C2cApplication {
@@ -10,4 +13,10 @@ public class C2cApplication {
         SpringApplication.run(C2cApplication.class, args);
     }
 
+
+    // encoding 에 필요한 encoder 를 빈 생성
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
